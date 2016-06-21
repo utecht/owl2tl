2dff19471efab3d5fa3d47fd04fc2490a32d2af9
Joseph Utecht <thenmal@gmail.com>
2016-06-21 15:11:31 -0500

    added caching behavior

--- owl2tl.py
+++ owl2tl.py
@@ -26,6 +26,13 @@ def get_hash(sha=''):
        return flask.redirect(flask.url_for('index')) 
     return flask.render_template('loading.html')
 
+@app.route('/<string:sha>/refresh', methods=['GET'])
+def refresh_data(sha=''):
+    if not sha:
+       return flask.redirect(flask.url_for('index')) 
+    redis_server.delete(sha + '/data')
+    return flask.redirect(flask.url_for('get_hash', sha=sha))
+
 @app.route('/<string:sha>/data', methods=['GET'])
 def load_data(sha=''):
     if not sha:
@@ -35,20 +42,31 @@ def load_data(sha=''):
     return wordlist(search['url'], search['annotations'])
 
 def check_hash(url, annotations):
-    sha_string = url + '.'.join(annotations)
-    sha = hashlib.sha256(sha_string.encode('utf-8')).hexdigest()[:8]
+    sha = get_sha(url, annotations)
     if not redis_server.exists(sha):
         redis_server.set(sha, pickle.dumps({'url': url, 'annotations': annotations}))
     return flask.redirect(flask.url_for('get_hash', sha=sha))
 
 def wordlist(url, annotations):
-    print(annotations)
-    uris = []
-    for annotation in annotations:
-        if annotation:
-            uris.append(annotation)
-    results = read_owl_file(url, uris)
+    sha = get_sha(url, annotations)
+    if redis_server.exists(sha + '/data'):
+        raw = redis_server.get(sha + '/data')
+        results = pickle.loads(raw)
+    else:
+        uris = []
+        for annotation in annotations:
+            if annotation:
+                uris.append(annotation)
+        results = read_owl_file(url, uris)
+        redis_server.set(sha + '/data', pickle.dumps(results))
+        redis_server.expire(sha + '/data', 86400)
+    results['sha'] = sha
     return flask.render_template('wordlist.html', results=results)
+
+def get_sha(url, annotations):
+    sha_string = url + '.'.join(annotations)
+    return hashlib.sha256(sha_string.encode('utf-8')).hexdigest()[:8]
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5050)
